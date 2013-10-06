53740447d2c03ff02316cff10a775037b39e2714
Volodymyr Sapsai <vsapsai@gmail.com>
2013-10-06 16:36:03 +0300

    Use gradient colors instead of random colors.

--- flamegraph.py
+++ flamegraph.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import operator
 import random
 import re
 import sys
@@ -161,6 +162,159 @@ version="1.1" xmlns="http://www.w3.org/2000/svg">"""
         stream.write(self._FOOTER)
 
 
+class Color:
+    @staticmethod
+    def rgb(*components):
+        return RGBColor(*components)
+
+    @staticmethod
+    def lab(*components):
+        return LabColor(*components)
+
+    @staticmethod
+    def xyz(*components):
+        return XYZColor(*components)
+
+    def _native_components(self):
+        assert False, "Should implement in subclasses"
+
+    def as_rgb(self):
+        assert False, "Should implement in subclasses"
+
+    def as_lab(self):
+        assert False, "Should implement in subclasses"
+
+    def as_xyz(self):
+        assert False, "Should implement in subclasses"
+
+    def rgb_string(self):
+        rgb_components = self.as_rgb()._native_components()
+        return "rgb({0}, {1}, {2})".format(*rgb_components)
+
+    def lab_components(self):
+        return self.as_lab()._native_components()
+
+    def _multiply_matrix_vector(self, matrix, vector):
+        assert len(matrix[0]) == len(vector)
+        result = [reduce(operator.add, map(operator.mul, row, vector))
+                  for row in matrix]
+        return result
+
+
+class RGBColor(Color):
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def _native_components(self):
+        return self.r, self.g, self.b
+
+    def as_rgb(self):
+        return self
+
+    def as_lab(self):
+        return self.as_xyz().as_lab()
+
+    def as_xyz(self):
+        # See http://en.wikipedia.org/wiki/SRGB
+        components = self._native_components()
+        components = [c / 255.0 for c in components]
+        # WARNING: gamma correction isn't performed, assume that RGB means not sRGB, but linear RGB
+        rgb_to_xyz_coefficients = ((0.4124, 0.3576, 0.1805),
+                                   (0.2126, 0.7152, 0.0722),
+                                   (0.0193, 0.1192, 0.9505))
+        xyz_components = self._multiply_matrix_vector(rgb_to_xyz_coefficients, components)
+        return Color.xyz(*xyz_components)
+
+
+class LabColor(Color):
+    def __init__(self, l, a, b):
+        self.l = l
+        self.a = a
+        self.b = b
+
+    def _native_components(self):
+        return self.l, self.a, self.b
+
+    def as_rgb(self):
+        return self.as_xyz().as_rgb()
+
+    def as_lab(self):
+        return self
+
+    def as_xyz(self):
+        # See http://en.wikipedia.org/wiki/Lab_color_space
+        l, a, b = self._native_components()
+        y = (l + 16.0) / 116.0
+        x = y + a / 500.0
+        z = y - b / 200.0
+        def f_inverse(t):
+            if t > (6.0 / 29.0):
+                return t ** 3
+            else:
+                return 3 * ((6.0 / 29.0) ** 2) * (t - (4.0 / 29.0))
+        xyz_components = map(lambda c, ref_c: ref_c * f_inverse(c), (x, y, z), XYZColor._WHITE_POINT_REF)
+        return Color.xyz(*xyz_components)
+
+
+class XYZColor(Color):
+    _WHITE_POINT_REF = (0.95043, 1.00000, 1.08890)
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def _native_components(self):
+        return self.x, self.y, self.z
+
+    def as_rgb(self):
+        # See http://en.wikipedia.org/wiki/SRGB
+        components = self._native_components()
+        xyz_to_rgb_coefficients = ((3.2406, -1.5372, -0.4986),
+                                   (-0.9689, 1.8758, 0.0415),
+                                   (0.0557, -0.2040, 1.0570))
+        rgb_components = self._multiply_matrix_vector(xyz_to_rgb_coefficients, components)
+        # WARNING: again, no gamma correction
+        rgb_components = [int(c * 255) for c in rgb_components]
+        return Color.rgb(*rgb_components)
+
+    def as_lab(self):
+        # See http://en.wikipedia.org/wiki/Lab_color_space
+        components = self._native_components()
+        components = map(lambda c, ref_c: c / ref_c, components, XYZColor._WHITE_POINT_REF)
+        def f(t):
+            if t > ((6.0 / 29.0) ** 3):
+                return t ** (1.0 / 3.0)
+            else:
+                return (1.0 / 3.0) * ((29.0 / 6.0) ** 2) * t + (4.0 / 29.0)
+        components = [f(c) for c in components]
+        x, y, z = components
+        l = 116.0 * y - 16.0
+        a = 500.0 * (x - y)
+        b = 200.0 * (y - z)
+        return Color.lab(l, a, b)
+
+    def as_xyz(self):
+        return self
+
+
+class ColorInterpolator:
+    def __init__(self, color_from, color_to):
+        self.color_from = color_from.as_lab()
+        self.color_to = color_to.as_lab()
+
+    def color_at_pos(self, position):
+        assert 0.0 <= position <= 1.0
+        from_components = self.color_from.lab_components()
+        to_components = self.color_to.lab_components()
+        assert len(from_components) == len(to_components)
+        result_components = map(lambda from_c, to_c: (from_c + position * (to_c - from_c)),
+                                from_components, to_components)
+        return Color.lab(*result_components)
+
+
 class ColorGenerator:
     def __init__(self, base_color_triplet, max_deviation_triplet):
         self.base_color = base_color_triplet
@@ -190,16 +344,20 @@ def main():
     thread_trace = report.process_trace.threads[0]
     sample_height = 16.
     width = 1200
-    height = sample_height * thread_trace.max_stack_depth()
+    max_stack_depth = thread_trace.max_stack_depth()
+    height = sample_height * max_stack_depth
     svg = SVG(width, height)
-    color_generator = ColorGenerator((180, 115, 28), (25, 115, 28))
+    #color_generator = ColorGenerator((180, 115, 28), (25, 115, 28))
+    color_interpolator = ColorInterpolator(Color.rgb(0xff, 0xed, 0xa0),
+                                           Color.rgb(0xf0, 0x3b, 0x20))
     width_per_sample = width / thread_trace.root_frame.sample_count
     # Draw samples' rectangles.
     for frame, start, depth in thread_trace.root_frame.iteritems():
         x = start * width_per_sample
         y = height - depth * sample_height
         width = frame.sample_count * width_per_sample
-        svg.add_rect(x, y - sample_height, width, sample_height - 1., color_generator.get_color_as_string())
+        svg.add_rect(x, y - sample_height, width, sample_height - 1.,
+                     color_interpolator.color_at_pos(float(depth) / max_stack_depth).rgb_string())
         svg.add_text(frame.frame, x + 2., y - 4.)
     svg.dump(sys.stdout)
 
